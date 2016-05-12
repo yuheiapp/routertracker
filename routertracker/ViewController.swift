37eccc5fb505f2436edb16b1574f4f989c4412f8
Yu Hei Leung <yuhei.app.dev@gmail.com>
2016-05-13 00:01:52 +0800

    init

---
+++ routertracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  routertracker
+//
+//  Created by Yu Hei Leung on 12/05/2016.
+//  Copyright © 2016 Yu Hei Leung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
